63da22f3e27e770c012f9451e500bfbb48e4fe86
hamdi <hamdi.chalbi@t4ulabs.com>
2023-10-11 16:11:54 +0100

    update watchgogReciever file

--- android/src/main/java/id/flutter/flutter_background_service/WatchdogReceiver.java
+++ android/src/main/java/id/flutter/flutter_background_service/WatchdogReceiver.java
@@ -17,41 +17,6 @@ import androidx.core.content.ContextCompat;
 public class WatchdogReceiver extends BroadcastReceiver {
     private static final int QUEUE_REQUEST_ID = 111;
     private static final String ACTION_RESPAWN = "id.flutter.background_service.RESPAWN";
-
-    public static void enqueue(Context context) {
-        enqueue(context, 5000);
-    }
-
-    public static void enqueue(Context context, int millis) {
-        Intent intent = new Intent(context, WatchdogReceiver.class);
-        intent.setAction(ACTION_RESPAWN);
-        AlarmManager manager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-
-        int flags = PendingIntent.FLAG_UPDATE_CURRENT;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            flags |= PendingIntent.FLAG_MUTABLE;
-        }
-
-        PendingIntent pIntent = PendingIntent.getBroadcast(context, QUEUE_REQUEST_ID, intent, flags);
-
-        // Check is background service every 5 seconds
-        AlarmManagerCompat.setExact(manager, AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + millis, pIntent);
-    }
-
-    public static void remove(Context context) {
-        Intent intent = new Intent(context, WatchdogReceiver.class);
-        intent.setAction(ACTION_RESPAWN);
-
-        int flags = PendingIntent.FLAG_CANCEL_CURRENT;
-        if (SDK_INT >= Build.VERSION_CODES.S) {
-            flags |= PendingIntent.FLAG_MUTABLE;
-        }
-
-        PendingIntent pi = PendingIntent.getBroadcast(context, WatchdogReceiver.QUEUE_REQUEST_ID, intent, flags);
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-        alarmManager.cancel(pi);
-    }
-
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(ACTION_RESPAWN)) {
